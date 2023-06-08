ee248cd5cf43efb2fad597be834adae9ca02d143
Jarvis-AI007 <pankajsharma9596708685@gmail.com>
2023-06-08 22:05:49 +0530

    Nav-Translation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+// import i18n (needs to be bundled ;))
+import "./i18n";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
