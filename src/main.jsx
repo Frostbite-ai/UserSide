e5f6a952f60291b7c0ea0b788be4cc7bd5c4d3c1
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-06-09 21:40:40 +0530

    fix DialogflowMessenger

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,13 @@ import App from "./App";
 // import i18n (needs to be bundled ;))
 import "./i18n";
 
+const dfMessengerScript = document.createElement("script");
+dfMessengerScript.src =
+  "https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1";
+dfMessengerScript.async = true;
+
+document.body.appendChild(dfMessengerScript);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
